98b1d2f1f0ed00970d7894ca5cc546940237047e
Yuliya Reshetneva <y.v.reshetneva@gmail.com>
2022-08-25 23:31:54 +0200

    Added TaskFourTestThree

--- demo/src/test/java/amazon/TaskFourTestThree.java
+++ demo/src/test/java/amazon/TaskFourTestThree.java
@@ -1,5 +1,6 @@
 package amazon;
 
+import com.google.common.collect.Ordering;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,11 +11,11 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-public class TaskFourTestTwo {
+public class TaskFourTestThree {
 
 
     private WebDriver driver;
@@ -27,7 +28,7 @@ public class TaskFourTestTwo {
     }
 
     @Test
-    public void testTwo(){
+    public void testThree(){
 
 
         driver.get("https://www.amazon.com/s?k=school+supplies&crid=3B2SMJ2SQ64N&pd_rd_r=002c7e59-b3e2-4870-9de7-a9c54fb9263c&pd_rd_w=9XU4m&pd_rd_wg=DkrTY&pf_rd_p=c0f33dcc-b6ee-417c-b438-e358c4a5e8dc&pf_rd_r=BSVWZBX1JYQGKW4WR8F4&sprefix=school+su%2Caps%2C175&ref=pd_gw_unk");
@@ -40,26 +41,40 @@ public class TaskFourTestTwo {
         WebElement maxPrice = driver.findElement(By.id("high-price"));
         maxPrice.sendKeys(max.toString(), Keys.ENTER);
 
+        //click link Sort By:
+        WebElement sortBy = driver.findElement(By.xpath("//span[contains(text(),'Sort by:')]")); // find by Xpath
+        Assert.assertTrue(sortBy.isDisplayed()); // Check/test/assert
+        sortBy.click();
+
+        //select sort from Low to High
+        WebElement sortAsc = driver.findElement(By.xpath("//a[@id='s-result-sort-select_1']")); // Find by Xpath
+        Assert.assertTrue(sortAsc.isDisplayed()); // Check/test/assert
+        sortAsc.click();
+
         //list prices elements
         List<WebElement> webElements = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
 
-        Set<String> prices = webElements.stream()
+        //get list prices (String) from the list of all items to buy
+        List<String> pricesStr = webElements.stream()
                 .map(e -> e.getText())
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
+
+        List<Integer> pricesInt = new ArrayList<>();
 
         //convert String prices to Integer prices
-        // and check price in filtered range [min, max]
-        for (String price : prices) {
-
+        for (String price : pricesStr) {
             try {
                 int priceInt = Integer.valueOf(price);
-                Assert.assertTrue(priceInt >= min);
-                Assert.assertTrue(priceInt <= max);
+                pricesInt.add(priceInt);
             } catch (NumberFormatException ex) {
+                //skip empty price
                 continue;
             }
 
         }
+
+        //check sorting/ordering
+        Assert.assertTrue(Ordering.natural().isOrdered(pricesInt));
     }
 
 
@@ -69,5 +84,3 @@ public class TaskFourTestTwo {
         driver.quit();
     }
 }
-
-
