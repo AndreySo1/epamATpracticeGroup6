4af35e2762e06e12f6bd3c783608d3e67d01a2ed
AndreySo1 <andrey.kafka@gmail.com>
2022-09-01 12:23:38 +0300

    init: start task

---
+++ demo/src/main/java/amazon/pageobject/pages/task2_search_field_functionality/HomePage.java
@@ -0,0 +1,13 @@
+package amazon.pageobject.pages.task2_search_field_functionality;
+
+import org.openqa.selenium.WebDriver;
+
+import amazon.pageobject.BasePage;
+
+public class HomePage extends BasePage{
+
+   public HomePage(WebDriver webDriver) {
+      super(webDriver);
+   }
+   
+}
