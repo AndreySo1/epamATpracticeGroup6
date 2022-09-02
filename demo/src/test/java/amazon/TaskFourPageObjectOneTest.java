be71885043910ecdc62b693ca27eb058b53f524b
Yuliya Reshetneva <y.v.reshetneva@gmail.com>
2022-09-02 09:49:54 +0200

    testFilterByPrice

--- demo/src/test/java/amazon/TaskFourPageObjectOneTest.java
+++ demo/src/test/java/amazon/TaskFourPageObjectOneTest.java
@@ -13,6 +13,8 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TaskFourPageObjectOneTest {
 
@@ -23,6 +25,9 @@ public class TaskFourPageObjectOneTest {
 
     @FindBy(id = "twotabsearchtextbox")
     private WebElement searchField;
+
+    @FindBy(xpath = "//span[@class='a-price-whole']")
+    private List<WebElement> filteredBYPrice;
 
     public TaskFourPageObjectOneTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
@@ -52,6 +57,51 @@ public class TaskFourPageObjectOneTest {
         }
     }
 
+
+    @Test
+    public void testFilterByPrice() {
+
+        driver.get("https://www.amazon.com/s?" +
+                "k=school+supplies" +
+                "&crid=3B2SMJ2SQ64N" +
+                "&pd_rd_r=002c7e59-b3e2-4870-9de7-a9c54fb9263c" +
+                "&pd_rd_w=9XU4m&pd_rd_wg=DkrTY" +
+                "&pf_rd_p=c0f33dcc-b6ee-417c-b438-e358c4a5e8dc" +
+                "&pf_rd_r=BSVWZBX1JYQGKW4WR8F4" +
+                "&sprefix=school+su%2Caps%2C175" +
+                "&ref=pd_gw_unk");
+
+        Integer min = 5;
+        WebElement lowPrice = driver.findElement(By.id("low-price"));
+        lowPrice.sendKeys(min.toString());
+
+        Integer max = 6;
+        WebElement maxPrice = driver.findElement(By.id("high-price"));
+        maxPrice.sendKeys(max.toString(), Keys.ENTER);
+
+        Set<String> prices = filteredBYPrice.stream()
+                .map(e -> e.getText())
+                .collect(Collectors.toSet());
+
+        //convert String prices to Integer prices
+        // and check price in filtered range [min, max]
+        for (String price : prices) {
+
+            try {
+                int priceInt = Integer.valueOf(price);
+
+                if (priceInt < min || priceInt > max) {
+                    continue;
+                }
+
+                Assert.assertTrue(priceInt >= min);
+                Assert.assertTrue(priceInt <= max);
+            } catch (NumberFormatException ex) {
+                continue;
+            }
+
+        }
+    }
 
     @AfterTest
     public void afterTest(){
