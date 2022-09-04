2953a49d40763832c4512a1ed5bce1903d996e15
“Vitali <essse.quam.videri@gmail.com>
2022-09-04 22:02:15 +0200

    JKanash - Page Object for Task #1

--- demo/src/main/java/properties/PropertyHolder2.java
+++ demo/src/main/java/properties/PropertyHolder2.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class PropertyHolder {
+public class PropertyHolder2 {
     private final Properties property = new Properties();
 
     {
